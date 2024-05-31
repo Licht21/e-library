fe654eb7961f108003b6b3bb8aaae99b6108b72e
yusuf <yusuf210409@gmail.com>
2024-05-31 09:03:18 +0700

    Memperbaiki url gambar

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <div class="container">
             <a class="navbar-brand" href="javascript:void(0)">
-                <img src="assets/img/pm-logo-sm.png" class="img-fluid img-logo">
+                <img src="asset/img/pm-logo-sm.png" class="img-fluid img-logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                 <span class="navbar-toggler-icon"></span>
