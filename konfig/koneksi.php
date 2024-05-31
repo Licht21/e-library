e93f97dae445d17b6c461efdbac9148c63518ac0
Muhammad Yusuf Aulia <90823385+Licht21@users.noreply.github.com>
2024-05-31 10:44:36 +0700

    Update koneksi.php

--- konfig/koneksi.php
+++ konfig/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn =  mysqli_connect('localhost','root','','library123');
+$conn =  mysqli_connect('localhost','root','','library');
 // Check connection
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
